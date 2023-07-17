2f48d13e7f60b245f7d367356c7b5ce3e8237dd1
Jie Chen <jiechenmc@gmail.com>
2023-07-17 11:06:08 -0400

    impl security later :(

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,6 @@ import { onRequest } from "firebase-functions/v2/https";
 // The Firebase Admin SDK to access Firestore.
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { getAuth } from "firebase-admin/auth";
 
 initializeApp();
 
@@ -25,23 +24,18 @@ exports.update_presence = onRequest(async (req, res) => {
       break
     }
     case ("POST"): {
-      if (req.headers.authorization) {
-        const verifyRes = await getAuth().verifyIdToken(req.headers.authorization)
-        logger.info(verifyRes)
-        logger.info(`${name} presence updated`);
-        // Push the new message into Firestore using the Firebase Admin SDK.
-        const writeResult = await getFirestore()
-          .collection(`chats/${course}/${section}/room/users`)
-          .doc(uid).set({ name: name, photoURL: photoURL });
+      logger.info(`${name} presence updated`);
+      // Push the new message into Firestore using the Firebase Admin SDK.
+      const writeResult = await getFirestore()
+        .collection(`chats/${course}/${section}/room/users`)
+        .doc(uid).set({ name: name, photoURL: photoURL });
 
-        const updateTime = writeResult.writeTime.toDate();
+      const updateTime = writeResult.writeTime.toDate();
 
-        // Send back a message that we"ve successfully written the message
-        res.json({ result: `User with ID: ${uid} updated at ${updateTime}.` });
-      } else {
-        res.status(403).send("Forbidden")
-      }
-      break;
+      // Send back a message that we"ve successfully written the message
+      res.json({ result: `User with ID: ${uid} updated at ${updateTime}.` });
     }
+      break;
   }
+}
 });
