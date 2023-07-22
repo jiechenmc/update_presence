9d3a3ed888b280579aec152057a95dc63a92a3ed
Jie Chen <jiechenmc@gmail.com>
2023-07-22 17:09:44 -0400

    upload file to store done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,8 @@ import { onRequest } from "firebase-functions/v2/https";
 import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { onObjectFinalized } from "firebase-functions/v2/storage";
+import { getStorage } from "firebase-admin/storage"
+import { firestore } from "firebase-admin";
 
 initializeApp();
 
@@ -40,7 +42,36 @@ exports.update_presence = onRequest({ region: 'us-east1' }, async (req, res) => 
   }
 });
 
+interface CustomMetaData {
+  source: string
+  author: string
+  authorPhotoURL: string
+}
+
 exports.upload_file_to_chat = onObjectFinalized({ region: 'us-east1' }, async (event) => {
-  logger.log("Object Upload Event TRIGGERED")
-  logger.log(event.data.metadata)
+  const file = event.data;
+  const fileBucket = file.bucket
+  const filePath = file.name
+
+  logger.debug(filePath)
+  const bucket = getStorage().bucket(fileBucket);
+
+  const blob = bucket.file(filePath)
+
+  const fileSelfLink = file.selfLink as string
+  logger.debug(fileSelfLink)
+
+
+  await blob.exists()
+  const [metadata] = await blob.getMetadata()
+
+  const customMD: CustomMetaData = metadata?.metadata
+
+  logger.debug(metadata)
+  logger.debug(customMD)
+
+  const now = firestore.FieldValue.serverTimestamp()
+  await getFirestore()
+    .collection(customMD.source)
+    .doc().set({ author: customMD.author, authorPhotoURL: customMD.authorPhotoURL, content: blob.publicUrl(), firstCreated: now, lastUpdated: now });
 })
